466d2555e85638e99906e4ea9e6f81103f2d0ee6
beatrizbranfb <beatrizbfb@icloud.com>
2026-09-03 19:05:25 -0300

    feat: adicionando o database

---
+++ APP/model/database.py
@@ -0,0 +1,11 @@
+from supabase import create_client, Client
+import os
+from dotenv import load_dotenv
+
+dotenv.load_dotenv()
+
+
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
